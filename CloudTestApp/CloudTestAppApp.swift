89aff4d352ac5f33fdde9d0c44d5d59478fd8dff
Paweł Kaźmierczak <pawel.kazmierczak@cyfra.com.pl>
2024-01-30 13:21:34 +0100

    Initial Commit

---
+++ CloudTestApp/CloudTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudTestAppApp.swift
+//  CloudTestApp
+//
+//  Created by Kaźmierczak Paweł on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CloudTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
